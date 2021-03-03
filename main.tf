1db0442ef558429627cf15803666fed944bc732b
Gunjan Hardasmalani <hardasmalani.g@northeastern.edu>
2021-03-02 19:38:06 -0500

    Build security group, rds instance

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 provider "aws" {
-    region = var.region
+  region = var.region
 }
 
 variable "region" {
-    type = string
+  type = string
 }
 
 variable "cidr_block" {
-   type = string
+  type = string
 }
 
 variable "vpc_name" {
-   type = string
+  type = string
 }
 
 variable "subnet_name" { # required
@@ -19,14 +19,14 @@ variable "subnet_name" { # required
 }
 
 variable "subnet_zone_count" {
-  type = number
-  default = 3
+  type        = number
+  default     = 3
   description = "Number of subnet zones to be created"
 }
 
 variable "start" {
-  type = number
-  default = 0
+  type        = number
+  default     = 0
   description = "Start index of second octet in subnet cidr block"
 }
 
@@ -45,27 +45,77 @@ variable "route_table_cidr_block" {
   type = string
 }
 
+variable "ingressCIDRblock" {
+  type = list(any)
+}
+
+variable "egressCIDRblock" {
+  type = list(any)
+}
+
+variable "database_security_group_name" {
+  type = string
+}
+
+variable "s3_bucket_name" {
+  type = string
+}
+
+variable "db_instance_class" {
+  type = string
+}
+
+
+variable "db_identifier" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
+
+variable "db_port" {
+  type = string
+}
+
+variable "subnet_group_name" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "aws_launch_configuration_name" { # required
+  type = string
+}
+
+
 # VPC
 resource "aws_vpc" "vpc" {
-    cidr_block = var.cidr_block
-    enable_dns_hostnames = true
-    enable_dns_support = true
-    enable_classiclink_dns_support = true
-    assign_generated_ipv6_cidr_block = false  
-    tags = {
-      Name = var.vpc_name
-    }
+  cidr_block                       = var.cidr_block
+  enable_dns_hostnames             = true
+  enable_dns_support               = true
+  enable_classiclink_dns_support   = true
+  assign_generated_ipv6_cidr_block = false
+  tags = {
+    Name = var.vpc_name
+  }
 }
 
 # Subnets
 resource "aws_subnet" "subnet" {
-  count = var.subnet_zone_count
-  cidr_block = "10.${var.start}.${10+count.index}.0/24"
-  vpc_id = aws_vpc.vpc.id
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  count                   = var.subnet_zone_count
+  cidr_block              = "10.${var.start}.${10 + count.index}.0/24"
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
   tags = {
-    Name = var.subnet_name 
+    Name = var.subnet_name
   }
 }
 
@@ -81,7 +131,7 @@ resource "aws_internet_gateway" "internet_gateway" {
 resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.vpc.id
   route {
-    cidr_block = var.route_table_cidr_block  #keeping this static according to assignment
+    cidr_block = var.route_table_cidr_block #keeping this static according to assignment
     gateway_id = aws_internet_gateway.internet_gateway.id
   }
   tags = {
@@ -95,3 +145,161 @@ resource "aws_route_table_association" "route" {
   subnet_id      = element(aws_subnet.subnet.*.id, count.index)
   route_table_id = aws_route_table.route_table.id
 }
+
+#AWS Security group 
+resource "aws_security_group" "application_security_group" {
+  vpc_id = aws_vpc.vpc.id
+  name   = "application"
+
+  # allow ingress of port 22
+  ingress {
+    cidr_blocks = var.ingressCIDRblock
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+
+  # allow ingress of port 80
+  ingress {
+    cidr_blocks = var.ingressCIDRblock
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+
+  # allow ingress of port 443
+  ingress {
+    cidr_blocks = var.ingressCIDRblock
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = var.ingressCIDRblock
+
+  }
+
+  # allow egress of all ports
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.egressCIDRblock
+  }
+
+  tags = {
+    Name = "application"
+  }
+}
+
+
+#AWS Security group for MySql
+resource "aws_security_group" "database" {
+  name        = var.database_security_group_name
+  description = "Allow MySQL inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "MySQL"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+
+    security_groups = [
+      aws_security_group.application_security_group.id,
+    ]
+  }
+  # allow all outgoing traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "database"
+  }
+
+}
+
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket        = var.s3_bucket_name
+  acl           = "private"
+  force_destroy = true
+
+  lifecycle_rule {
+    id      = "log"
+    enabled = true
+    prefix = "log/"
+    tags = {
+      "rule"      = "log"
+      "autoclean" = "true"
+    }
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA" # or "ONEZONE_IA"
+    }
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  tags = {
+    Name = "S3 bucket"
+    # Environment = "dev"
+  }
+}
+
+#RDS
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = var.subnet_group_name
+  subnet_ids = aws_subnet.subnet.*.id
+}
+
+resource "aws_db_parameter_group" "db_parameter_ssl" {
+  name   = "rds-mysql-ssl"
+  family = "mysql5.7"
+
+  parameter {
+    name         = "performance_schema"
+    value        = "1"
+    apply_method = "pending-reboot"
+  }
+}
+
+resource "aws_db_instance" "rds_instance" {
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = var.db_instance_class
+  multi_az               = false
+  identifier             = var.db_identifier
+  username               = var.db_username
+  password               = var.db_password
+  port                   = var.db_port
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+  publicly_accessible    = false
+  name                   = var.db_name
+  vpc_security_group_ids = [aws_security_group.database.id]
+
+  # attach security group to RDS instance
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  parameter_group_name = aws_db_parameter_group.db_parameter_ssl.name
+  skip_final_snapshot = true
+  storage_encrypted  = true
+  ca_cert_identifier = "rds-ca-2019"
+}
+
+# resource "aws_launch_configuration" "launch_conf" {
+#    name = var.aws_launch_configuration_name
+#  }
